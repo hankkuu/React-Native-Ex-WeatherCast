4a163d879def96268cca6a3774c6015066104596
hankkuu <hankkuu@gmail.com>
2018-08-30 12:40:38 +0900

    update weather

--- App.js
+++ App.js
@@ -3,19 +3,24 @@ import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
 import Weather from './Weather';
 
+const API_KEY = "c391b7145cc1004b938016579e394943";
+
 export default class App extends Component {
   state = {
     isLoaded: false,     // 외부 API 호출로 이 부분을 컨트롤하게 되면 아래 View에서 화면이 보일지 말지 정해진다.    
-    error: null
+    error: null,
+    temperature: null,
+    name: null
   };
   // 앱이 시작될 때 마다 이 부분을 실행하는 것 같다 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        //console.log(position);
-        this.setState({
-          isLoaded: true
-        });
+        console.log(position);
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+        // this.setState({
+        //   isLoaded: true
+        // });
       },
       (error) => {
         console.log(error);
@@ -24,19 +29,40 @@ export default class App extends Component {
         })        
       },      
       // console.log("position"), 이유는 모르겠지만 위의 API를 사용할 수 없다... 일단 나중에알아보자
-       this.setState({
-         isLoaded: true
-       })
+      //  this.setState({
+      //    isLoaded: true
+      //  })
     );
   }
 
+  _getWeather = (lat, lon) => {
+    //console.log(API_KEY);
+    //let a = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`;
+    //console.log(a);
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`
+    ).then(response => response.json())
+     .then(json => {
+        console.log(json);
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        })
+     })
+     .catch(error => {
+       console.log(error.message);
+      throw error;
+    });
+  }
+
   render() {
-    const { isLoaded, error } = this.state;
+    const { isLoaded, error, temperature, name } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
         {isLoaded ? (
-            <Weather /> 
+            <Weather weatherName={name} temp={Math.floor(temperature - 273.15)} /> 
           ) : (
             <View style={styles.loading}>
               <Text style={styles.loadingText}>Getting the weather</Text>
--- Weather.js
+++ Weather.js
@@ -2,24 +2,95 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
+import PropsTypes from 'prop-types';
 
-export default class Weather extends Component {
-    render() {
-        return (
-            <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container} >            
-                <View style={styles.upper}>
-                    {/* expo에 있는 Icon을 사용하는 것으로 expo를 설치하면 사용할 수 있는 패키지이다  */}
-                    <Ionicons color="white" size={144} name="ios-rainy"></Ionicons> 
-                    <Text style={styles.temp}> 35도</Text>
-                </View>
-                <View style={styles.lower}>
-                    <Text style={styles.title}>Raining like a MF</Text>
-                    <Text style={styles.subTitle}>For more info look outside</Text>
-                </View>
-            </LinearGradient>
-        );
+// export default class Weather extends Component {
+//     render() {
+//         return (
+//             <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container} >            
+//                 <View style={styles.upper}>
+//                     {/* expo에 있는 Icon을 사용하는 것으로 expo를 설치하면 사용할 수 있는 패키지이다  */}
+//                     <Ionicons color="white" size={144} name="ios-rainy"></Ionicons> 
+//                     <Text style={styles.temp}> 35도</Text>
+//                 </View>
+//                 <View style={styles.lower}>
+//                     <Text style={styles.title}>Raining like a MF</Text>
+//                     <Text style={styles.subTitle}>For more info look outside</Text>
+//                 </View>
+//             </LinearGradient>
+//         );
+//     }
+// }
+
+const weatherCases = {
+    Rain: {
+        colors: ['#00C6FB', '#005BEA'],
+        title: "Raining",
+        subtitle: "우산챙기세요",
+        icon: 'ios-rainy'
+    },
+    Clear: {
+        colors: ['#FEF253', '#FF7300'],
+        title: "Sunny",
+        subtitle: "선크림 발라라",
+        icon: 'ios-sunny'
+    },
+    Thunderstorm: {
+        colors: ['#00ECBC', '#007ADF'],
+        title: "Thunderstorm",
+        subtitle: "번개 피해요",
+        icon: 'ios-thunderstorm'
+    },
+    Cloud: {
+        colors: ['#D7D2CC', '#304352'],
+        title: "Cloud",
+        subtitle: "날씨 좋다",
+        icon: 'ios-cloudy'
+    },
+    Snow: {
+        colors: ['#7DE2FC', '#B9B6E5'],
+        title: "Snow",
+        subtitle: "눈온다",
+        icon: 'ios-snow'
+    },
+    Dizzle: {
+        colors: ['#89F7FE', '#66A6FF'],
+        title: "Dizzle",
+        subtitle: "으악",
+        icon: 'dizzy'
+    },
+    Haze: {
+        colors: ['#89F7FE', '#66A6FF'],
+        title: "Haze",
+        subtitle: "으악",
+        icon: 'dehaze'
     }
+
 }
+
+Weather.propsTypes = {
+    temp: PropsTypes.number.isrequred,
+    weatherName: PropsTypes.string.isrequred
+}
+
+function Weather( { temp, weatherName } ) {
+    console.log(weatherName);
+    return (
+        <LinearGradient colors={weatherCases[weatherName].colors} style={styles.container} >
+            <View style={styles.upper}>
+                {/* expo에 있는 Icon을 사용하는 것으로 expo를 설치하면 사용할 수 있는 패키지이다  */}
+                <Ionicons color="white" size={144} name={weatherCases[weatherName].icon}></Ionicons>
+                <Text style={styles.temp}> {temp}도</Text>
+            </View>
+            <View style={styles.lower}>
+                <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+                <Text style={styles.subTitle}>{weatherCases[weatherName].subtitle}</Text>
+            </View>
+        </LinearGradient>
+    );
+}
+
+export default Weather;
 
 const styles = StyleSheet.create({
     container: {
